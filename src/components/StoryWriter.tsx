5414f69a8389d627d1aec93992e2e427008ad97c
anushkadeshpande <anushkaa5000@gmail.com>
2024-06-22 17:08:49 +0530

    feat: Added story generation ssection

--- src/components/StoryWriter.tsx
+++ src/components/StoryWriter.tsx
@@ -13,6 +13,11 @@ import { Button } from "./ui/button";
 const StoryWriter = () => {
   const [story, setStory] = useState("");
   const [pages, setPages] = useState<number>();
+  const [progress, setProgress] = useState("");
+  
+  
+  const [runStarted, setRunStarted] = useState<boolean>(false);
+  const [runFinished, setRunFinished] = useState<boolean | null>(null);
 
   return (
     <div className="flex flex-col container">
@@ -39,6 +44,21 @@ const StoryWriter = () => {
         </Select>
 
         <Button disabled={!story || !pages}>Generate Story 🚀</Button>
+      </section>
+
+
+      <section className="flex-1 pb-5 mt-5">
+        <div className="flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font-mono p-10 h-96 overflow-y-scroll">
+            <div>
+              {
+                runFinished === null && (<>
+                  <p className="animate-pulse mr-5">{"I'm waiting for you to write a prompt! 🐱"}</p>
+                </>) 
+              }
+              <span className="mr-5">{">>"}</span>
+              {progress}
+            </div>
+        </div>
       </section>
     </div>
   );
