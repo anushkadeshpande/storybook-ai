92e392f9091bd3f7078b4f2c4268580f61949a18
anushkadeshpande <anushkaa5000@gmail.com>
2024-07-22 06:19:00 +0530

    feat: Added backend code which invokes gptScript

--- src/app/api/run-script/route.ts
+++ src/app/api/run-script/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest } from "next/server";
 import { RunEventType, RunOpts } from "@gptscript-ai/gptscript"
+import gptScript from "@/lib/gptScriptInstance";
+
+const script = 'app/api/run-script/story-boot.gpt'
 
 export async function POST(request: NextRequest) {
   const {story, pages, path} = await request.json()
@@ -17,11 +20,25 @@ export async function POST(request: NextRequest) {
     const stream = new ReadableStream({
       async start(controller) {
         try {
+          const run = await gptScript.run(script, opts)
 
+          run.on(RunEventType.Event, (data) => controller.enqueue(encoder.encode(`event: ${JSON.stringify(data)}\n\n`)))
+
+          await run.text()
+
+          controller.close()
         } catch(error) {
           controller.error(error)
           console.log(error)
         }
+      }
+    })
+
+    return new Response(stream, {
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive"
       }
     })
   } catch(error) {
